06e1d20de2a5b5a57c79ba50b406eb7fdaf1ba42
Simon <57576661+simon-stentoft@users.noreply.github.com>
2025-02-21 15:18:44 +0100

    Livereload stuff dunno how it works honestly :clown:

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from livereload import Server
+
 app = Flask(__name__)
 
+#### Start server by typing 'python app.py' in the terminal
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -15,3 +18,11 @@ def contact():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    server = Server(app.wsgi_app)
+
+    # Watch files for changes and reload
+    server.watch('templates/*.html')
+    server.watch('static/*.css')
+
+    server.serve(port=5500)
