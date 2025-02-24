3854ffad954dda8982b8816af37e5fe2d912a7c6
Simon <57576661+simon-stentoft@users.noreply.github.com>
2025-02-24 16:07:08 +0100

    mobile index page done

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ app = Flask(__name__)
 #### Start server by typing 'python app.py' in the terminal
 @app.route("/")
 def index():
-    return render_template("index.html")
+    active_index = "active"
+    return render_template("index.html", active_index=active_index)
 
 @app.route("/menu")
 def menu():
-    return render_template("menu.html")
+    active_menu = "active"
+    return render_template("menu.html", active_menu=active_menu)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    active_contact = "active"
+    return render_template("contact.html", active_contact=active_contact)
 
 if __name__ == "__main__":
     app.run(debug=True)
